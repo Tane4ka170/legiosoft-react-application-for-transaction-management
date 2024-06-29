4d83c68168c4ca3b066ab2cff30a6fb3a584d6a2
Tane4ka170 <tane4ka170@gmail.com>
2024-06-30 01:11:34 +0300

    deleted unnesessary

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
